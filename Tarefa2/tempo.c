e2d8ac41b68348aa54c71621d65e60a27480ab73
Júlio Doebeli <35260298+JulioDoebeli@users.noreply.github.com>
2019-04-22 14:43:29 -0300

    Create tempo.c

---
+++ Tarefa2/tempo.c
@@ -0,0 +1,93 @@
+// tempo.c
+#include <stdio.h>
+#include <stdlib.h>
+#include "smpl.h"
+
+// Cada nodo vai contar o seu tempo
+// Eventos
+#define test 1
+#define fault 2
+#define repair 3
+
+// Descritor do nodo
+typedef struct {
+    int id;
+    int variavel;
+} tnodo;
+tnodo *nodo;
+
+///// PROGRAMA PRINCIPAL /////
+int main (int argc, char *argv[]) {
+    static int N,
+               token,
+               event,
+               r,
+               Prox,
+               i;
+    static char fa_name[5];
+
+    if (argc != 2) {
+        puts("Uso correto: tempo <num_nodos>");
+        exit(1);
+    }
+    N = atoi(argv[1]);
+    smpl(0,"Um exemplo de simulacao");
+    reset();
+    stream(1);
+
+    // Inicializacao
+    nodo = (tnodo *) malloc (sizeof(tnodo)*N);
+    for (i=0; i<N; i++) {
+        memset(fa_name, '\0', 5);
+        sprintf(fa_name, "%d", i);
+        nodo[i].id = facility(fa_name,1);
+        nodo[i].variavel = -1;
+        /*printf("fa_name = %s, nodo[i].id = %d\n", fa_name, nodo[i].id);*/
+    }
+
+    // Escalonamento de eventos iniciais
+    for (i=0; i<N; i++)
+        schedule(test, 30.0, i);
+    schedule(fault, 31.0, 2);
+    schedule(repair, 61.0, 2);
+
+    // Loop principal da simulacao
+    while (time() < 100.0) {
+        cause (&event, &token);
+        switch(event) {
+        case test:
+            if (status(nodo[token].id) != 0) break;
+            Prox = (token+1) % N;
+            printf("O nodo %d vai testar o nodo ", token);
+            printf("%d no tempo %5.1f ---> ", Prox, time());
+                if ( (status(nodo[Prox].id)) == 0)
+                {
+                    printf("   Nodo %d estah sem-falha.\n",Prox);
+                    if (nodo[Prox].variavel != -1)
+                        nodo[token].variavel = nodo[Prox].variavel;
+                    printf("   nodo[%d].variavel = %d\n", token, nodo[token].variavel);
+                }
+                else
+                {
+                    printf("   Nodo %d estah falho.\n",Prox);
+                    nodo[token].variavel = Prox;
+                    printf("   nodo[%d].variavel = %d\n", token, nodo[token].variavel);
+                }
+            schedule(test,30.0,token);
+            break;
+        case fault:
+            r = request(nodo[token].id, token, 0);
+            if (r != 0) {
+                puts("Nao foi possivel falhar");
+                exit(1);
+            }
+            printf("O nodo %d falhou no tempo %5.1f\n", token, time());
+            break;
+        case repair:
+            printf("O nodo %d recuperou no tempo %5.1f\n", token, time());
+            release(nodo[token].id, token);
+            schedule(test,30.0,token);
+            break;
+        }
+    }
+}
